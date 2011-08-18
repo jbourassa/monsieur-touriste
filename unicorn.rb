3ae3a5b057b463fccee50ab0e5a31345c100bc13
Jimmy Bourassa <jimmy.bourassa@hooktstudios.com>
2011-08-17 22:40:42 -0400

    fixed config for unicorn once for good

--- unicorn.rb
+++ unicorn.rb
@@ -4,12 +4,10 @@ working_directory app_root
 
 timeout 30
 
-# This is where we specify the socket.
-# We will point the upstream Nginx module to this socket later on
-listen app_root+"/tmp/unicorn.sock", :backlog => 64
+listen 'unix:' + app_root + '/tmp/unicorn.sock', :backlog => 64
 
 pid app_root+"/tmp/unicorn.pid"
 
-# Set the path of the log files inside the log folder of the testapp
+# Set the path of the log files inside the tmp folder
 stderr_path app_root+"/tmp/unicorn.stderr.log"
 stdout_path app_root+"/tmp/unicorn.stdout.log"
